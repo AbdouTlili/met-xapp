accbbcdd290337bf079e39e816c72fb3555f977e
AbdouTlili <ha_tlili@esi.dz>
2022-04-17 16:23:13 +0200

    topoapi test

--- cmd/onos-kpimon/met-xapp.go
+++ cmd/onos-kpimon/met-xapp.go
@@ -5,9 +5,11 @@
 package main
 
 import (
+	"context"
 	"flag"
 
-	"github.com/abdoutlili/met-xapp/pkg/manager"
+	"github.com/abdoutlili/met-xapp/pkg/rnib"
+	topoapi "github.com/onosproject/onos-api/go/onos/topo"
 	"github.com/onosproject/onos-lib-go/pkg/certs"
 	"github.com/onosproject/onos-lib-go/pkg/logging"
 )
@@ -18,14 +20,12 @@ func main() {
 	caPath := flag.String("caPath", "", "path to CA certificate")
 	keyPath := flag.String("keyPath", "", "path to client private key")
 	certPath := flag.String("certPath", "", "path to client certificate")
-	e2tEndpoint := flag.String("e2tEndpoint", "onos-e2t:5150", "E2T service endpoint")
-	ricActionID := flag.Int("ricActionID", 10, "RIC Action ID in E2 message")
-	configPath := flag.String("configPath", "/etc/onos/config/config.json", "path to config.json file")
-	grpcPort := flag.Int("grpcPort", 5150, "grpc Port number")
-	smName := flag.String("smName", "met-xapp", "Service model name in RAN function description")
-	smVersion := flag.String("smVersion", "v1", "Service model version in RAN function description")
-
-	ready := make(chan bool)
+	// e2tEndpoint := flag.String("e2tEndpoint", "onos-e2t:5150", "E2T service endpoint")
+	// ricActionID := flag.Int("ricActionID", 10, "RIC Action ID in E2 message")
+	// configPath := flag.String("configPath", "/etc/onos/config/config.json", "path to config.json file")
+	// grpcPort := flag.Int("grpcPort", 5150, "grpc Port number")
+	// smName := flag.String("smName", "met-xapp", "Service model name in RAN function description")
+	// smVersion := flag.String("smVersion", "v1", "Service model version in RAN function description")
 
 	flag.Parse()
 
@@ -35,19 +35,44 @@ func main() {
 	}
 
 	log.Info("Starting met-xapp")
-	cfg := manager.Config{
-		CAPath:      *caPath,
-		KeyPath:     *keyPath,
-		CertPath:    *certPath,
-		E2tEndpoint: *e2tEndpoint,
-		GRPCPort:    *grpcPort,
-		RicActionID: int32(*ricActionID),
-		ConfigPath:  *configPath,
-		SMName:      *smName,
-		SMVersion:   *smVersion,
+	// cfg := manager.Config{
+	// 	CAPath:      *caPath,
+	// 	KeyPath:     *keyPath,
+	// 	CertPath:    *certPath,
+	// 	E2tEndpoint: *e2tEndpoint,
+	// 	GRPCPort:    *grpcPort,
+	// 	RicActionID: int32(*ricActionID),
+	// 	ConfigPath:  *configPath,
+	// 	SMName:      *smName,
+	// 	SMVersion:   *smVersion,
+	// }
+
+	rnibClient, err := rnib.NewClient()
+	ctx, _ := context.WithCancel(context.Background())
+	watchE2Connections(ctx, rnibClient)
+
+}
+
+func watchE2Connections(ctx context.Context, rnibClient rnib.Client) error {
+	ch := make(chan topoapi.Event)
+	err := rnibClient.WatchE2Connections(ctx, ch)
+	if err != nil {
+		log.Warn(err)
+		return err
 	}
 
-	mgr := manager.NewManager(cfg)
-	mgr.Run()
-	<-ready
+	// creates a new subscription whenever there is a new E2 node connected and supports KPM service model
+	for topoEvent := range ch {
+		log.Debugf("Received topo event: %v", topoEvent)
+
+		if topoEvent.Type == topoapi.EventType_ADDED || topoEvent.Type == topoapi.EventType_NONE {
+			relation := topoEvent.Object.Obj.(*topoapi.Object_Relation)
+			e2NodeID := relation.Relation.TgtEntityID
+			if !rnibClient.HasKPMRanFunction(ctx, e2NodeID, "1.3.6.1.4.1.53148.1.2.2.97") {
+				continue
+			}
+		}
+
+	}
+	return nil
 }
---
+++ pkg/rnib/rnib.go
@@ -0,0 +1,160 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package rnib
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/onosproject/onos-lib-go/pkg/errors"
+	"github.com/onosproject/onos-lib-go/pkg/logging"
+
+	topoapi "github.com/onosproject/onos-api/go/onos/topo"
+	toposdk "github.com/onosproject/onos-ric-sdk-go/pkg/topo"
+)
+
+var log = logging.GetLogger()
+
+// TopoClient R-NIB client interface
+type TopoClient interface {
+	WatchE2Connections(ctx context.Context, ch chan topoapi.Event) error
+	GetCells(ctx context.Context, nodeID topoapi.ID) ([]*topoapi.E2Cell, error)
+	GetE2NodeAspects(ctx context.Context, nodeID topoapi.ID) (*topoapi.E2Node, error)
+	E2NodeIDs(ctx context.Context) ([]topoapi.ID, error)
+}
+
+// NewClient creates a new topo SDK client
+func NewClient() (Client, error) {
+	sdkClient, err := toposdk.NewClient()
+	if err != nil {
+		return Client{}, err
+	}
+	cl := Client{
+		client: sdkClient,
+	}
+	return cl, nil
+}
+
+// Client topo SDK client
+type Client struct {
+	client toposdk.Client
+}
+
+func (c *Client) HasMETRanFunction(ctx context.Context, nodeID topoapi.ID, oid string) bool {
+	e2Node, err := c.GetE2NodeAspects(ctx, nodeID)
+	if err != nil {
+		log.Warn(err)
+		return false
+	}
+
+	for _, sm := range e2Node.GetServiceModels() {
+		log.Warn("the SM OID is :", sm.OID, sm.Name)
+		if sm.OID == "1.3.6.1.4.1.53148.1.2.2.97" {
+			log.Warn("this is the one we are suposed to send a sub request to")
+			return true
+		}
+	}
+	return false
+}
+
+// GetCellTopoID gets cell topo ID with cell object ID
+func (c *Client) GetCellTopoID(ctx context.Context, coi string, nodeID topoapi.ID) (topoapi.ID, error) {
+	cells, err := c.GetCells(ctx, nodeID)
+	if err != nil {
+		return "", err
+	}
+
+	for _, cell := range cells {
+		if coi == cell.CellObjectID {
+			return topoapi.ID(fmt.Sprintf("%s/%s", string(nodeID), cell.CellGlobalID.Value)), nil
+		}
+	}
+	return "", errors.NewNotFound("E2Cell not found with CellObjectID")
+}
+
+// E2NodeIDs lists all of connected E2 nodes
+func (c *Client) E2NodeIDs(ctx context.Context) ([]topoapi.ID, error) {
+	objects, err := c.client.List(ctx, toposdk.WithListFilters(getControlRelationFilter()))
+	if err != nil {
+		return nil, err
+	}
+
+	e2NodeIDs := make([]topoapi.ID, len(objects))
+	for _, object := range objects {
+		relation := object.GetRelation()
+		e2NodeID := relation.TgtEntityID
+		e2NodeIDs = append(e2NodeIDs, e2NodeID)
+	}
+
+	return e2NodeIDs, nil
+}
+
+// GetE2NodeAspects gets E2 node aspects
+func (c *Client) GetE2NodeAspects(ctx context.Context, nodeID topoapi.ID) (*topoapi.E2Node, error) {
+	object, err := c.client.Get(ctx, nodeID)
+	if err != nil {
+		return nil, err
+	}
+	e2Node := &topoapi.E2Node{}
+	err = object.GetAspect(e2Node)
+	return e2Node, err
+
+}
+
+// GetCells get list of cells for each E2 node
+func (c *Client) GetCells(ctx context.Context, nodeID topoapi.ID) ([]*topoapi.E2Cell, error) {
+	filter := &topoapi.Filters{
+		RelationFilter: &topoapi.RelationFilter{
+			SrcId:        string(nodeID),
+			RelationKind: topoapi.CONTAINS,
+			TargetKind:   topoapi.E2CELL,
+		},
+	}
+
+	objects, err := c.client.List(ctx, toposdk.WithListFilters(filter))
+	if err != nil {
+		return nil, err
+	}
+	var cells []*topoapi.E2Cell
+	for _, obj := range objects {
+		cellObject := &topoapi.E2Cell{}
+		err = obj.GetAspect(cellObject)
+		if err != nil {
+			log.Warn("Cell entity %s has no E2Cell aspect", obj.ID)
+			return nil, err
+		}
+		cells = append(cells, cellObject)
+	}
+
+	if len(cells) == 0 {
+		return nil, errors.New(errors.NotFound, "there is no cell to subscribe for e2 node %s", nodeID)
+	}
+
+	return cells, nil
+}
+
+func getControlRelationFilter() *topoapi.Filters {
+	controlRelationFilter := &topoapi.Filters{
+		KindFilter: &topoapi.Filter{
+			Filter: &topoapi.Filter_Equal_{
+				Equal_: &topoapi.EqualFilter{
+					Value: topoapi.CONTROLS,
+				},
+			},
+		},
+	}
+	return controlRelationFilter
+}
+
+// WatchE2Connections watch e2 node connection changes
+func (c *Client) WatchE2Connections(ctx context.Context, ch chan topoapi.Event) error {
+	err := c.client.Watch(ctx, ch, toposdk.WithWatchFilters(getControlRelationFilter()))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+var _ TopoClient = &Client{}
